9413aa8a22b1838e50659de8ee5068a0a53c2c8f
Maulik <maulik.des@gmail.com>
2019-10-16 22:32:07 +0530

    created simple dashboard

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module("AngularMaterialDemo",['ngMaterial'])
-    .run(function(){
+angular.module("AngularMaterialDemo", ['ngMaterial'])
+    .run(function () {
         console.log("loaded successfully");
     });
